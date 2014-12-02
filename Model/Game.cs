c7038295ddb3b29944452a698b0618f36b925799
Ramona <chong.lu@uqconnect.edu.au>
2014-12-02 15:57:07 +1000

    Created Game class and constructor

---
+++ Model/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Game
+    {
+        public Game (int rows, int columns){
+
+        }
+    }
+}
